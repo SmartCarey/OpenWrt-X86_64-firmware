3a9f57c26a04c6490562b8a4bb2a4892d059a06c
Carey <38372470+SmartCarey@users.noreply.github.com>
2021-02-06 00:57:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-git clone https://github.com/jerrykuku/lua-maxminddb.git /package
-git clone https://github.com/jerrykuku/luci-app-vssr.git /package
-git clone https://github.com/kenzok8/openwrt-packages.git /package
-git clone https://github.com/kenzok8/small /package
+git clone https://github.com/jerrykuku/lua-maxminddb.git package
+git clone https://github.com/jerrykuku/luci-app-vssr.git package
+git clone https://github.com/kenzok8/openwrt-packages.git package
+git clone https://github.com/kenzok8/small package
